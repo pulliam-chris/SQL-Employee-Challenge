e84ffbcf1c2ffa92dfe8cce2d39a8ac31412549f
Chris Pulliam <chrispulliam.jobs@gmail.com>
2021-02-24 16:07:02 -0800

    more work on the SQL analysis queries

--- EmployeeSQL/DataAnalysis.sql
+++ EmployeeSQL/DataAnalysis.sql
@@ -1,4 +1,3 @@
-
 --List the following details of each employee: employee number, last name, first name, sex, and salary.
 SELECT s.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM salaries AS s
@@ -12,7 +11,10 @@ WHERE hire_date BETWEEN DATE('01/01/1986') AND DATE('12/31/1986')
 ORDER BY hire_date;
 
 --List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-
+SELECT dm.dept_no, d.dept_name, dm.emp_no
+FROM dept_mgr as dm
+JOIN departments as d ON
+dm.dept_no=d.dept_no
 
 --List the department of each employee with the following information: employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
@@ -26,8 +28,15 @@ de.dept_no=d.dept_no;
 SELECT e.first_name, e.last_name, e.sex
 FROM employees as e
 WHERE e.first_name = 'Hercules' AND SUBSTRING(e.last_name,1,1) = 'B';
---List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees as e
+JOIN dept_emp as de ON
+e.emp_no=de.emp_no
+JOIN departments as d ON
+de.dept_no=d.dept_no
+WHERE de.dept_no = 'd007';
 
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
